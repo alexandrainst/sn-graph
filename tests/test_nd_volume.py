e8ed87fb804eb77987b2699f949f42cc5cc6be4a
Tomasz Prytula <tomasz.prytula@alexandra.dk>
2025-03-20 13:35:49 +0100

    Add some content to the test_nd_volume.

--- tests/test_nd_volume.py
+++ tests/test_nd_volume.py
@@ -2,22 +2,36 @@ import numpy as np
 import sn_graph as sn
 
 
-def test_nd_volume() -> None:
-    def create_nd_array_with_cube(n: int) -> np.ndarray:
-        # Create n-dimensional array of size 150 in every dimension, filled with zeros
-        shape = tuple([50] * n)
-        arr = np.zeros(shape)
-        # Start and end indices for the cube in each dimension
-        start_idx = 10
-        end_idx = 40
-        # Create slices for each dimension
-        slices = tuple(slice(start_idx, end_idx) for _ in range(n))
-        # Set the central cube to ones
-        arr[slices] = 1
-        return arr
+def create_nd_array_with_cube(n: int) -> np.ndarray:
+    # Create n-dimensional array of size 150 in every dimension, filled with zeros
+    shape = tuple([50] * n)
+    arr = np.zeros(shape)
+    # Start and end indices for the cube in each dimension
+    start_idx = 10
+    end_idx = 40
+    # Create slices for each dimension
+    slices = tuple(slice(start_idx, end_idx) for _ in range(n))
+    # Set the central cube to ones
+    arr[slices] = 1
+    return arr
 
+
+def test_nd_volume() -> None:
     for n in range(1, 5):
         cube_array = create_nd_array_with_cube(n)
         assert cube_array.shape == (50,) * n
         sn_graph = sn.create_sn_graph(cube_array)
         assert isinstance(sn_graph, tuple)
+
+        vertices, _ = sn_graph
+
+        # assert len(vertices) >= 1, "The graph should have at least one vertex"
+        # assert isinstance(vertices[0], tuple), "The vertex should be a tuple"
+        # assert (
+        #     len(vertices[0]) == n
+        # ), "The vertex should have the same number of dimensions as the input array"
+        # assert set(
+        #     vertices[0]
+        # ).issubset(
+        #     {24, 25}
+        # ), "The first vertex should be in the middle of the cube, so its coordinates should be ((10+39)/2)= (24 or 25), in every dimension"
