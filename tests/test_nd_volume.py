5a4b40d5116f864e23469e1521ddf089ea968494
Tomasz Prytula <tomasz.prytula@alexandra.dk>
2025-03-24 12:50:16 +0100

    Move text to assertion.

--- tests/test_nd_volume.py
+++ tests/test_nd_volume.py
@@ -33,5 +33,7 @@ def test_nd_cube(dim: int, make_cube: Callable[[int], np.ndarray]) -> None:
     assert len(vertices) >= 1, "There should be at least one vertex in the graph"
     assert isinstance(vertices[0], tuple), "The first vertex should be a tuple"
     assert len(vertices[0]) == dim, "The first vertex should have n coordinates"
-    # The coordinates of the first vertex should be in the middle of the cube.
-    assert set(vertices[0]).issubset({24, 25})
+
+    assert set(vertices[0]).issubset(
+        {24, 25}
+    ), "The coordinates of the first vertex should be in the middle of the cube"
