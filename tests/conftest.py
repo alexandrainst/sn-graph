52fe8022b9b77ad053defe17841d48b8847e7a05
Tomasz Prytula <tomasz.prytula@alexandra.dk>
2025-04-02 23:04:54 +0200

    Add a conftest.

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import sys
+import os
+
+# Add the parent directory to the Python path
+# This makes the main package importable in the tests
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
