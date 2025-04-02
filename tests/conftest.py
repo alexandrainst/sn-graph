4b5c0a340dcca1baed4259070a9b5aad58bdb6ba
Tomasz Prytula <tomasz.prytula@alexandra.dk>
2025-04-02 23:23:46 +0200

    Remove conftest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,3 @@
-import os
-import sys
-
 # Add the parent directory to the Python path
 # This makes the main package importable in the tests
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
