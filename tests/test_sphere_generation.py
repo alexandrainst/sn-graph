161830739dea71c8f9419a639af442fb73c092c4
Tomasz Prytula <tomasz.prytula@alexandra.dk>
2025-05-21 10:12:06 +0200

    Add a unit test for _choose_next_sphere.

--- tests/test_sphere_generation.py
+++ tests/test_sphere_generation.py
@@ -45,3 +45,47 @@ def test_no_sphere_is_chosen_cause_because_no_candidates(
         sdf_array, current_spheres, current_candidates
     )
     assert (next_sphere, new_candidates) == (None, None)
+
+
+# There is some gigantic messup problem here in how the cnadidates are passed around and returned: it comes as candiadates coordinates but is returned as a boolean mask. I think the whole function has to be refactored.
+@pytest.mark.parametrize(
+    (
+        "sdf_array",
+        "current_spheres",
+        "current_candidates",
+        "expected_sphere",
+        "expected_candidates",
+    ),
+    [
+        (
+            np.ones((10, 10)),
+            [(1, 1)],
+            np.array([[3, 3]]),
+            (np.int64(3), np.int64(3)),
+            np.array([True]),
+        ),  # choose the only available candidate
+        (
+            np.ones((10, 10)),
+            [(1, 1)],
+            np.array([[3, 3], [4, 4]]),
+            (np.int64(4), np.int64(4)),
+            np.array([True, True]),
+        ),  # choose the further of the two as all the spheres have radius 1
+    ],
+)  # type: ignore[misc]
+def test_next_sphere_is_chosen_correctly_and_candidates_are_updated(
+    sdf_array: np.ndarray,
+    current_spheres: List[Tuple[int, int]],
+    current_candidates: np.ndarray,
+    expected_sphere: Tuple[int, int],
+    expected_candidates: np.ndarray,
+) -> None:
+    next_sphere, new_candidates = sn.core._choose_next_sphere(
+        sdf_array, current_spheres, current_candidates
+    )
+    assert (
+        next_sphere == expected_sphere
+    ), "The next sphere should be the one with the maximum distance from the current spheres, givven they all have the same radius"
+    assert (
+        new_candidates == expected_candidates
+    ).all(), "The new candidates mask should be the same as the expected ones"
